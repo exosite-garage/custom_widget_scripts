e2e14a1abbf6adf7e0c84a69a7e002786fc3494b
Michael Aanenson <mikeaanenson@exosite.com>
2014-06-18 16:30:00 -0500

    adding folder with readme and image in addition to the widget javascript file

--- line_graphs/line_graph_using_flot/flot_line_graph_show_selected_data.js
+++ line_graphs/line_graph_using_flot/flot_line_graph_show_selected_data.js
@@ -1,7 +1,14 @@
 /**
  * Example Exosite Custom Widget - Flot line graph
- * http://www.flotcharts.org/
+ * Uses: http://www.flotcharts.org/
  * @version 1
+ * This example custom widget is meant to be like the off the shelf 
+ * line graph widgets available in Exosite portals, but allow developers 
+ * flexibility to change the code.
+ * The widget assumes data will be provided to it using the Widget container as an array 
+ * and that it will be refreshed by the widget configuration refresh time.
+ * Some of the flot options are commented out below so developers can quickly see how they 
+ * may do something like formatting the date, specifying a Y axis range, etc.
  */
 
 // select one or more data sources from the list above to view this demo.
@@ -11,7 +18,7 @@ function(container, portal)
     
     var graph_initialized = false; //only init once
     var widgetid = getWidgetInfo("id");
-    var html_graph = "graph";
+    var html_graph = "graph" + String(widgetid);
     var html_graph_element = "#"+html_graph+"";
    console.log("html element: " + html_graph);
 
@@ -27,10 +34,12 @@ function(container, portal)
         //timeformat: "%m/%d/%Y",
         //timeformat: "%b %d",
         //timeformat: "%I:%M:%S %p",
-        selection: { mode: "x" },
-        grid: { hoverable: true, clickable: true },
+        //selection: { mode: "x" },
+        //yaxis:{min:0,max:100}, //if you want to hard-code y axis range
+        grid: { hoverable: false, clickable: false },
         pan: { interactive: false },
-        colors: ["#41C4DC","#D5E04D","#FF5847","#FFC647", "#5D409C", "#BF427B" ]
+        colors: ["#41C4DC","#FF5847","#FFC647", "#5D409C", "#BF427B","#D5E04D" ], // pick your own or comment out for defaults
+        //grid: { backgroundColor:"#FFFFFF"}
     };
 
     /*****
@@ -40,7 +49,7 @@ function(container, portal)
     // prints an error message if something fails in the widget.
     function errorMsg(message)
     {
-        $("#graph").html(message);    
+        $(html_graph_element).html(message);    
     }
 
     // returns a matrix of time and data.
@@ -51,17 +60,17 @@ function(container, portal)
         function initGraph()
         {
             console.log('init graph');
-            $("#graph").bind("plotselected", function (event, ranges) {
+            $(html_graph_element).bind("plotselected", function (event, ranges) {
                 // do the zooming
                 //console.log('zoom in, stop refresh')
-                $.plot($("#graph"), [series],
+                $.plot($(html_graph_element), [series],
                         $.extend(true, {}, detailOptions, {
                         xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to }       
                 }));
             });
 
             var previousPoint = null;
-            $("#graph").bind("plothover", function (event, pos, item) {
+            $(html_graph_element).bind("plothover", function (event, pos, item) {
                 if (item) 
                 {
                     var x = new Date(item.datapoint[0]);
@@ -84,6 +93,7 @@ function(container, portal)
 
         var current_time = parseInt((new Date).getTime() / 1000);
         //var window_start = current_time-TIME_HISTORY;
+        var current_value = null;
 
         if (graph_initialized == false){
             //console.log('init flot');
@@ -96,6 +106,7 @@ function(container, portal)
 
         //console.log('update graph')
         var series = [];
+        //Get all selected device dataports
         for (i = 0; i < portal.clients.length; i++)
         {
             //console.log('getting data for:' + portal.clients[i].alias );
@@ -104,71 +115,118 @@ function(container, portal)
 
                 var meta = portal.clients[i].dataports[j]['info']['description']['meta'];
                 var friendly = portal.clients[i].dataports[j]['info']['description']['name'];
-                console.log('got data for: ' + friendly )
+                var meta_json = JSON.parse(meta);
+                var dp_units = '';
+                if (meta_json['datasource']['unit'] ){
+                    dp_units = meta_json['datasource']['unit'];
+                }
+                console.log('got data for: ' + friendly );
                 series.push({
                     label: friendly,
                     data: portal.clients[i].dataports[j]['data'],
-                    hoverable: true
+                    hoverable: true,
+                    units: dp_units
                 });
             }
 
         }
-        // call the Portals 'read' widget API
+        //get all selected portal dataports
         for (j = 0; j < portal.dataports.length; j++)
         {
 
             var meta = portal.dataports[j]['info']['description']['meta'];
             var friendly = portal.dataports[j]['info']['description']['name'];
-            console.log('got data for: ' + friendly )
+            var meta_json = JSON.parse(meta);
+            var dp_units = '';
+            if (meta_json['datasource']['unit'] ){
+                dp_units = meta_json['datasource']['unit'];
+            }
+            console.log('got data for: ' + friendly );
             series.push({
                 label: friendly,
                 data: portal.dataports[j]['data'],
-                hoverable: true
+                hoverable: true,
+                units: dp_units
             });
         }
 
-        console.log('check if we have data')
+        console.log('check if we have data');
         // if no data, show user message instead of graph
-        var nodata = false;
-        for (var j = 0; j < series.length-1;  j++ )
+        console.log('series length:' + series.length)
+
+        if (series.length == 0) {
+            console.log('no data for time window')
+            errorMsg('No data within realtime data time window or no data selected');
+            //timer_id_graphrefersh = setTimeout(flotGraph, 2000);
+            return;
+        }
+        var nodata = true;
+        for (var j = 0; j < series.length;  j++ )
         {
-            if (0 == series[j]['data'].length) {nodata = true;}
-            else {nodata = false;}
+            if (0 < series[j]['data'].length) {
+                nodata = false;
+            }
+            //console.log('data points set size:'+ series[j]['label'] + ',' + series[j]['data'].length);
         }
         if (nodata == true){
+            console.log('no data for time window');
             errorMsg('No data within realtime data time window');
-            timer_id_graphrefersh = setTimeout(flotGraph, 2000);
+            //timer_id_graphrefersh = setTimeout(flotGraph, 2000);
             return;
         }
 
         //make sure data is in timeseries order
-        console.log('sort data')
         for (var j = series.length-1; j >= 0 ; j-- )
         {
-            series[j]['data'].sort(function(a,b){return a[0] - b[0]}); //Array now becomes [7, 8, 25, 41]
-                                  
-            //Change Unix Timestamps to Javascript milliseconds timestamp
-            for (var z = 0; z < series[j]['data'].length; z++)
-            {
-                series[j]['data'][z][0] = series[j]['data'][z][0]*1000;
-            }
 
-            // end value (real-time) should be null so graph leading (right) edge stays up to current time
-            if (series[j]['data'][series[j]['data'].length-1][0] < current_time){
-                //console.log('set leading value to null');
-                series[j]['data'].push([current_time,null]);
+            if (series[j]['data'].length > 0)
+            {
+                // Sort data to be sure in correct time order
+                console.log('sort data')
+                series[j]['data'].sort(function(a,b){return a[0] - b[0]}); //Array now becomes [7, 8, 25, 41]
+                
+                // get the last value
+                current_value = series[j]['data'][series[j]['data'].length-1][1]
+
+                // end value (real-time) should be null so graph leading (right) edge stays up to current time
+                if (series[j]['data'][series[j]['data'].length-1][0] < current_time){
+                    console.log('set leading value to null');
+                    series[j]['data'].push([current_time,null]);
+                }
+
+                /*
+                // start value (real-time) should be null so graph historical edge stays up to current time window
+                // Unfortunately the time window is not known, it is not available from the options for the widget.
+                // You could specify your own window start / size 
+                if (time_series_data[j].timedata[0][0] > window_start){
+                    //console.log('start gap in data');
+                    time_series_data[j].timedata.splice(0,0,[window_start,null]); 
+                }
+                */
+
+                //Change Unix Timestamps to Javascript milliseconds timestamp
+                for (var z = 0; z < series[j]['data'].length; z++)
+                {
+                    series[j]['data'][z][0] = series[j]['data'][z][0]*1000;
+                }
+
+                //put the current value into the label
+                if (current_value != null){
+                    series[j]['label'] = series[j]['label'] + ':  ' + String(current_value) + ' ' + series[j]['units']; //latest value in label
+                }
             }
-            /*
-            // start value (real-time) should be null so graph historical edge stays up to current time window
-            if (time_series_data[j].timedata[0][0] > window_start){
-                //console.log('start gap in data');
-                time_series_data[j].timedata.splice(0,0,[window_start,null]); 
+            else {
+                 //put the 'na' into the label
+                series[j]['label'] = series[j]['label'] + ':  na'; //latest value in label
             }
-            */
+        }
+        if (series.length == 1){
+            // if only showing one line graph, use fill
+            detailOptions['series']['lines']['fill'] = true;
         }
 
         //console.log('graphing updated data');
-        $.plot($("#graph"), series, detailOptions);
+        $.plot($(html_graph_element), series, detailOptions);
 
     }
 
@@ -199,8 +257,8 @@ function(container, portal)
         }
         else
         {
-            html += '<div id="graphwidget" style="width:90%; height:90%;">';
-                html += '<div id="' + html_graph + '" style="margin:20px; width:100%; height:100%; text-align:center;">';
+            html += '<div id=\"graphwidget\" style=\"width:95%; height:90%;\">';
+                html += '<div id=\"' + html_graph + '\" class=\"graph\" style=\"margin-top:18px; margin-left:15px; width:100%; height:100%; text-align:center; position:relative\">';
                 html += 'just a sec...loading some data...';
                 html += '</div>';
             html += '</div>';
---
+++ line_graphs/line_graph_using_flot/readme.md
@@ -0,0 +1,7 @@
+__ Summary __
+
+This reference Javascript duplicates the off the shelf Line Graph widget as a custom widget, making a call to the Flot graphing library. Developers can modify to fit their needs with all of the options available to them in the code.  This code assumes that the data and dataport information will be provided on each widget container refresh.
+
+__ Image __ 
+
+![image](screen_shot_line_graph.png)
